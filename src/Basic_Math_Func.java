3aa56a2c07b1d9e0f0c30cc2b645074a85cadd2c
Benny1610-yes <benedict.newton10@gmail.com>
2024-02-05 16:11:24 +0000

    first update

---
+++ src/Basic_Math_Func.java
@@ -0,0 +1,34 @@
+public class Basic_Math_Func {
+
+    public int basic_int_addition(int a, int b){
+        return a + b;
+    }
+
+    public int basic_int_subtraction(int a, int b){
+        return a - b;
+    }
+
+    public int basic_int_multiplication(int a, int b){
+        return a * b;
+    }
+
+    public int basic_int_division(int a, int b){
+        return a / b;
+    }
+
+    public float basic_float_addition(float a, float b){
+        return a + b;
+    }
+
+    public float basic_float_subtraction(float a, float b){
+        return a - b;
+    }
+
+    public float basic_float_multiplication(float a, float b){
+        return a * b;
+    }
+
+    public float basic_float_division(float a, float b){
+        return a / b;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,12 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.println("main");
+        Basic_Math_Func e = new Basic_Math_Func();
+
+        System.out.println(e.basic_int_addition(1, 2));
+    }
+}
