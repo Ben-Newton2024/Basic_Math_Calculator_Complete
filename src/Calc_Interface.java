e6cae4829ae9128a230578fc7a7dec29ec96cd54
Benny1610-yes <benedict.newton10@gmail.com>
2024-02-06 16:53:44 +0000

    Started the basis of main calc face going through sorted equation list ot do basic math operations onto it, then appending it back to the list. and repeating till its just a digit answer.

--- src/Calc_Interface.java
+++ src/Calc_Interface.java
@@ -1,10 +1,13 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.Scanner;
 
 public class Calc_Interface {
 
     public void main_calc_interface(){
+
+        Basic_Math_Func Basic_Math_Functions = new Basic_Math_Func();
 
         Scanner in = new Scanner(System.in);
         // read console input line
@@ -16,10 +19,47 @@ public class Calc_Interface {
 
         // now complete the math onto this list above
 
+        for (int i = 0; i <= equation_list.size()-1; i++){
+
+
+
+
+
+            if( !Character.isDigit(Integer.parseInt(equation_list.get(i)))){
+                // if the item in position "i" is found to not be a digit when made into a integer, then it must be an
+                // operator.
+                // if it is an operator then we can take the digits from either side of it and use the operator to do
+                // what math needs to be done on it then
+
+                if (Objects.equals(equation_list.get(i), "+")){
+                    // add the digits from either side of the equations list together.
+                    int a = Integer.parseInt(equation_list.get(i-1));
+                    int b = Integer.parseInt(equation_list.get(i+1));
+                    int c = Basic_Math_Functions.basic_int_addition(a, b);
+
+                    // answer c is found and can be added to the list, this can be done by removing the current operator
+                    // and the two digits on either side of it, shortening the list.
+
+                    // replacing the operator with the answer
+                    equation_list.set(i, String.valueOf(c));
+
+                    // now removing the digits on either side of that "operator" now answer digit
+                    equation_list.remove(i-1);
+                    //removed position before i, therefore list is now 1 position smaller
+                    equation_list.remove(i);
+
+                    //reset "i" to 0 as we have edited the list and need to ensure that it goes back through the entire
+                    // list correctly with no blank positions or reset positions that duplicate.
+                    i = 0;
+                }
+            }
+
+
+        }
     }
 
     public List<String> equation_sorting(String input_equation){
-        // need to check to see if the inputed equation has the correct syntax, e.g, not end in a + or -, make sure there
+        // need to check to see if the inputted equation has the correct syntax, e.g, not end in a + or -, make sure there
         // are enough ( to match the amount of )
 
 
